d969e931053ed65e695b31a4cdc749df3b5003be
Rafael Alonso <rafael.palonso@gmail.com>
2020-05-04 09:29:54 -0300

    Passed all repository tests (without csv)

---
+++ lib/cookbook.rb
@@ -0,0 +1,21 @@
+class Cookbook
+
+  def initialize
+    @recipes = []
+  end
+
+  def all
+    # entrega todas as receitas que estou guardando
+    @recipes
+  end
+
+  def add_recipe(new_recipe)
+    @recipes << new_recipe
+  end
+
+  def remove_recipe(recipe_index)
+    # delete_at remove um elemento da array baseado no ínidice dele
+    @recipes.delete_at(recipe_index)
+  end
+
+end
